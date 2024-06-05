a03f6702e8984c0bd639e828cb2f8d29a18aa836
DamithraFdo <damithrafdo@gmail.com>
2024-06-05 22:51:33 +0530

    add the uno fite to git

---
+++ Vesak_Lantern_Programming.ino
@@ -0,0 +1,58 @@
+/*
+ * StrydoLabs All Right Recived|strydolabs@gmail.com|damithrafdo@gmail.com|+94716507322|
+ * Vesak Lantern Relay Programming *8 Pattern
+ * Hardware Specification: Arduino UNO, 8Ch Relay
+ */
+void setup() {
+  pinMode(2,OUTPUT); //Relay line 01
+  pinMode(3,OUTPUT); //Relay line 02
+  pinMode(4,OUTPUT); //Relay line 03
+  pinMode(5,OUTPUT); //Relay line 04
+  pinMode(6,OUTPUT); //Relay line 05
+  pinMode(7,OUTPUT); //Relay line 06
+  pinMode(8,OUTPUT); //Relay line 07
+  pinMode(9,OUTPUT); //Relay line 08
+}
+
+void loop() {
+  Pattern1();
+  delay(10);
+  Pattern2();
+  delay(10);
+  Pattern3();
+  delay(10);
+  Pattern4();
+  delay(10);
+  Pattern5();
+  delay(10);
+  Pattern6();
+  delay(10);
+  Pattern7();
+  delay(10);
+  Pattern8();
+  delay(10);
+}
+void Pattern1(){
+  
+}
+void Pattern2(){
+  
+}
+void Pattern3(){
+  
+}
+void Pattern4(){
+  
+}
+void Pattern5(){
+  
+}
+void Pattern6(){
+  
+}
+void Pattern7(){
+  
+}
+void Pattern8(){
+  
+}
